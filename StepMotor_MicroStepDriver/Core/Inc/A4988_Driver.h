bf1754dc1d9126c211ee44008598e890eee7eeae
LYH-94 <a0936223829@gmail.com>
2021-11-06 00:49:09 +0800

    first version

---
+++ StepMotor_MicroStepDriver/Core/Inc/A4988_Driver.h
@@ -0,0 +1,25 @@
+/*
+ * A4988_Driver.h
+ *
+ *  Created on: Oct 6, 2021
+ *      Author: LIN
+ */
+
+#ifndef INC_STEPMOTORPWM_H_
+#define INC_STEPMOTORPWM_H_
+
+
+
+#endif /* INC_STEPMOTORPWM_H_ */
+
+#include "main.h"
+#include "math.h"
+
+#define PWM_TIMER_CLOCKS 84000000  // Timer Frequency (Hz)
+#define PWM_PSC (PWM_TIMER_CLOCKS/1000000)-1  // Prescaler
+#define MICRO_STEP_RESOLUTION 16  // 1=Full step, 2=Halft step, 4=Quarter step, 8=Eighth step or 16=Sixteenth step
+
+void StepMotorDriver_Iint(TIM_HandleTypeDef *htim);
+uint32_t StepMotor_Control(TIM_HandleTypeDef *htim, float angular_velocity, float degree);
+void StepMotor_Stop(TIM_HandleTypeDef *htim);
+
---
+++ StepMotor_MicroStepDriver/Core/Src/A4988_Driver.c
@@ -0,0 +1,94 @@
+/*
+ * A4988_Driver.c
+ *
+ *  Created on: Oct 6, 2021
+ *      Author: LIN
+ */
+
+#include "A4988_Driver.h"
+#include "main.h"
+
+void StepMotorDriver_Iint(TIM_HandleTypeDef *htim)
+{
+	HAL_GPIO_WritePin(GPIOA, PA9_DIR_Pin, GPIO_PIN_SET);  // StepMotor direction
+	HAL_GPIO_WritePin(GPIOA, PA8_ENA_Pin, GPIO_PIN_SET);  // StepMotor output DISABLE
+
+	switch(MICRO_STEP_RESOLUTION)
+	{
+	case 16:
+		HAL_GPIO_WritePin(GPIOB, PB13_MS1_Pin, GPIO_PIN_SET);  // StepMotor microstep MS1 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB14_MS2_Pin, GPIO_PIN_SET);  // StepMotor microstep MS2 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB15_MS3_Pin, GPIO_PIN_SET);  // StepMotor microstep MS3 , HIGH
+		//printf("microstep = 16\n");
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOB, PB13_MS1_Pin, GPIO_PIN_SET);  // StepMotor microstep MS1 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB14_MS2_Pin, GPIO_PIN_SET);  // StepMotor microstep MS2 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB15_MS3_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS3 , LOW
+		//printf("microstep = 8\n");
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOB, PB13_MS1_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS1 , LOW
+		HAL_GPIO_WritePin(GPIOB, PB14_MS2_Pin, GPIO_PIN_SET);  // StepMotor microstep MS2 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB15_MS3_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS3 , LOW
+		//printf("microstep = 4\n");
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOB, PB13_MS1_Pin, GPIO_PIN_SET);  // StepMotor microstep MS1 , HIGH
+		HAL_GPIO_WritePin(GPIOB, PB14_MS2_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS2 , LOW
+		HAL_GPIO_WritePin(GPIOB, PB15_MS3_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS3 , LOW
+		//printf("microstep = 2\n");
+		break;
+	default:
+		HAL_GPIO_WritePin(GPIOB, PB13_MS1_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS1 , LOW
+		HAL_GPIO_WritePin(GPIOB, PB14_MS2_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS2 , LOW
+		HAL_GPIO_WritePin(GPIOB, PB15_MS3_Pin, GPIO_PIN_RESET);  // StepMotor microstep MS3 , LOW
+		//printf("microstep = 1\n");
+		break;
+	}
+
+	__HAL_TIM_SET_PRESCALER(htim, PWM_PSC);
+	//printf("PWM_PSC = %d\n", PWM_PSC);
+}
+
+/**
+  * brief
+  *
+  * note
+  *
+  * param - htim ： Is the timer
+  * param - angular_velocity ： The angular velocity of the motor (unit:degree/sec)
+  * param - degree ： Motor rotation angle (unit:degree)
+  * return - step
+  */
+uint32_t StepMotor_Control(TIM_HandleTypeDef *htim, float angular_velocity, float degree)
+{
+	if(degree < 0)
+	{
+		HAL_GPIO_WritePin(GPIOA, PA9_DIR_Pin, GPIO_PIN_RESET);
+		degree = -degree;
+	}
+	else
+		HAL_GPIO_WritePin(GPIOA, PA9_DIR_Pin, GPIO_PIN_SET);
+
+	if(angular_velocity < 1.71661376953125) angular_velocity = 1.71661376953125;
+	if(angular_velocity > 1081.7307692307692307692307692308) angular_velocity = 1081.7307692307692307692307692308;
+	uint16_t PWM_ARR = lroundf(1000000/(angular_velocity/(1.8/MICRO_STEP_RESOLUTION)))-1;
+	if(PWM_ARR < 103) PWM_ARR = 103;
+	if(PWM_ARR > 65535) PWM_ARR = 65535;
+	//printf("PWM_ARR = %d\n", PWM_ARR);
+
+	__HAL_TIM_SET_AUTORELOAD(htim, PWM_ARR);
+	__HAL_TIM_SET_COMPARE(htim, TIM_CHANNEL_1, (__HAL_TIM_GET_AUTORELOAD(htim) + 1)/2);
+
+	HAL_GPIO_WritePin(GPIOA, PA8_ENA_Pin, GPIO_PIN_RESET);
+	HAL_TIM_PWM_Start_IT(htim, TIM_CHANNEL_1);
+
+	return lroundf(degree/(1.8/MICRO_STEP_RESOLUTION));
+}
+
+void StepMotor_Stop(TIM_HandleTypeDef *htim)
+{
+	//HAL_GPIO_WritePin(GPIOA, PA8_ENA_Pin, GPIO_PIN_SET);
+	HAL_TIM_PWM_Stop_IT(htim, TIM_CHANNEL_1);
+}
